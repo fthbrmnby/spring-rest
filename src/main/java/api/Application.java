8284030ce78d25df05f1a665f7ba0a75a1772be2
Fatih Barmanbay <fatih.barmanbay@exastax.com>
2017-12-22 09:58:00 +0300

    Moved the main class and renamed

---
+++ src/main/java/api/Application.java
@@ -0,0 +1,15 @@
+package api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Fatih Barmanbay at 21.12.2017
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
