5ca0eeea025501f199dd05009d5a314d1a471304
Fatih Barmanbay <fatih.barmanbay@exastax.com>
2017-12-22 10:04:09 +0300

    Replaced Morphia with Spring Data's Mongo repository

---
+++ src/main/java/api/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package api.repository;
+
+import api.entity.UserEntity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<UserEntity, String> {
+
+}
